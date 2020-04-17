b515daf75acde5114320d5f62d049a51d27de70a
unknown <dovanloc2466@gmail.com>
2020-04-17 14:35:28 +0700

    add wellcome content exemple

--- src/Component/Main/WellCome/WellCome.tsx
+++ src/Component/Main/WellCome/WellCome.tsx
@@ -1,12 +1,100 @@
 import * as React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Card } from "antd";
 
 export const WellCome = () => {
     document.title = "Dashboard";
     return (
-        <Row>
-            <Col>
-                <h1>Hello Lộc, how are you feeling today!!! </h1>
+        <Row gutter={[30, 30]}>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </Col>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </Col>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </Col>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </Col>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </Col>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
+            </Col>
+            <Col span={6}>
+                <div className="site-card-border-less-wrapper">
+                    <Card
+                        title="Card title"
+                        bordered={false}
+                        style={{ width: 300 }}
+                    >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </div>
             </Col>
         </Row>
     );
